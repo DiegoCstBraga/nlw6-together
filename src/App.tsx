d476bbba84d18ea800a44c6e43130b124c195daf
Diego da Costa Braga <diego.cstbraga@gmail.com>
2021-06-24 04:22:03 -0300

    Dia 3 - Nova Sala e Perguntas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { Room } from "./pages/Room";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<AuthContextProvider>
-				<Route path="/" exact component={Home} />
-				<Route path="/rooms/new" component={NewRoom} />
+                {/* Switch serve para apresentar uma rota por vez, sem a necessidade de procurar por outras */}
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/rooms/new" component={NewRoom} />
+					<Route path="/rooms/:id" component={Room} />
+				</Switch>
 			</AuthContextProvider>
 		</BrowserRouter>
 	);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,11 +8,14 @@ import { Button } from "../components/Button";
 
 import "../styles/auth.scss";
 import { useAuth } from "../hooks/useAuth";
+import { FormEvent } from "react";
+import { useState } from "react";
+import { database } from "../services/firebase";
 
 export function Home() {
 	const history = useHistory();
-
-	const { user, signInWithGoogle } = useAuth()
+	const { user, signInWithGoogle } = useAuth();
+	const [roomCode, setRoomCode] = useState("");
 
 	async function handleCreateRoom() {
 		if (!user) {
@@ -20,6 +23,24 @@ export function Home() {
 		}
 
 		history.push("/rooms/new");
+	}
+
+	async function handleJoinRoom(event: FormEvent) {
+		// Evita que o formulário direcione para outra página instantaneamente
+		event.preventDefault();
+
+		if(roomCode === ''){
+			return
+		}
+
+		const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+		if (!roomRef.exists()){
+			alert('Room does not exists.')
+			return
+		}
+
+		history.push(`/rooms/${roomCode}`)
 	}
 
 	return (
@@ -43,8 +64,13 @@ export function Home() {
 
 					<div className="separator">ou entre em uma sala</div>
 
-					<form>
-						<input type="text" placeholder="Digite o código da sala" />
+					<form onSubmit={handleJoinRoom}>
+						<input
+							type="text"
+							placeholder="Digite o código da sala"
+							onChange={event => setRoomCode(event.target.value)}
+							value={roomCode}							
+						/>
 						<Button type="submit">Entrar na sala</Button>
 					</form>
 				</div>
